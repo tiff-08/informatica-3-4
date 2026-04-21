9b0befd03f1a021c7168ebab50a6894ab039f1c6
tiff-08 <tiffanynicsp@gmail.com>
2026-04-21 14:54:55 +0000

    finished work

---
+++ naming-variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number; //i change the ! after the number
+int test_value; // i put the _ between test and value
+int example1; // i change the one to the end
+float int_value; // i added _value next to int
+printf("Variables created successfully.\n");
+}
