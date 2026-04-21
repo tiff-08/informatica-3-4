578d31c0a280785c9fdce1c56fa457482c67fa4b
tiff-08 <tiffanynicsp@gmail.com>
2026-04-21 15:48:30 +0000

    Machine print

---
+++ vending.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //Variables for Tortas Chuma
+    float price1 = 74.99;
+    int quantity1 = 15;
+    char select1 = 't';
+
+    //Variables for Doritos
+    float price2 = 20.99;
+    int quantity2 = 18;
+    char select2 = 'd';
+
+    //Variables for Oreo
+    float price3 = 18.99;
+    int quantity3 = 20;
+    char select3 = 'o';
+
+    //print stock of vending machine
+    printf("Vending Machine Stock\n");
+    printf("--------------------------\n");
+    printf("Item name: Tortas Chuma\n");
+    
+
+}
