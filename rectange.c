5be9ab232ef2e0327b65b35c5e37fc6795613f2c
tiff-08 <tiffanynicsp@gmail.com>
2026-04-24 16:37:47 +0000

    rectange not finished

---
+++ rectange.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    int width;
+
+    printf("Enter the height of the rectangle \n");
+    printf("Enter the width of the rectangle \n");
+
+    int area=width*height;
+    int perimeter=width*height;
+
+}
