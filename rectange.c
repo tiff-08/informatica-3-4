8da238d05b150da85bf8e20a2733e6624d4ec158
tiff-08 <tiffanynicsp@gmail.com>
2026-04-27 16:37:54 +0000

    rect

--- rectange.c
+++ rectange.c
@@ -2,13 +2,10 @@
 
 int main(void)
 {
-    int height;
+    int length;
     int width;
 
-    printf("Enter the height of the rectangle \n");
-    printf("Enter the width of the rectangle \n");
-
-    int area=width*height;
-    int perimeter=width*height;
+    printf("Enter the height of the rectangle: \n");
+    printf("Enter the width of the rectangle: \n");
 
 }
