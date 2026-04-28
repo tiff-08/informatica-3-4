778721d5724d82b2474afd570767c89d9723fa53
tiff-08 <tiffanynicsp@gmail.com>
2026-04-28 15:48:38 +0000

    movies not finished

---
+++ movies.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int Minutes;
+    int Starttime;
+    printf("Enter run time of the movie: \n");
+    scanf("%d", &Minutes);
+    printf("Enter start time: \n");
+    scanf("%d", &Starttime);
+    printf("\n");
+    int Runtime= Minutes/60;
+    int Runtime2= Minutes%60;
+    int Endtime= Starttime+Runtime;
+    int Endtime2= Starttime+Runtime2;
+    printf("The movie will last %d hours and %d minutes\n", Runtime, Runtime2);
+    printf("End Time %d:%d \n", Endtime, Endtime2);
+}
