e36e6b8aa63cbb838448536208471a6c9952f7e5
tiff-08 <tiffanynicsp@gmail.com>
2026-04-29 14:54:50 +0000

    Test

---
+++ Test/receipt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int Apple;
+   int price1 = 2.00;
+   int quantity1 = 2;
+   printf("\n");
+
+   printf("Item: \n");
+   scanf("%d", &Apple);
+   printf("Price: \n");
+   scanf("%d", &price1);
+   printf("Quantity bought: \n");
+   scanf("%d", &quantity1);
+
+   printf("\n");
+   printf("********************\n");
+   printf("Techdraui Grocery Store\n");
+   printf("********************\n");
+}
